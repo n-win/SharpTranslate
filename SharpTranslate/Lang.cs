86562f86105c89b8b4f4f06bbb89bc021ef64a67
nwin <mail@nwin.me>
2021-02-27 22:57:24 -0500

    Start the project

---
+++ SharpTranslate/Lang.cs
@@ -0,0 +1,13 @@
+namespace SharpTranslate
+{
+    /// <summary>
+    /// A language, as represented by ISO 639-1 2-letter country code.
+    /// </summary>
+    public enum Lang
+    {
+        /// <summary>English (English)</summary>
+        EN,
+        /// <summary>Español (Spanish)</summary>
+        ES
+    }
+}
---
+++ SharpTranslate/Translator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SharpTranslate
+{
+    public static class Translator
+    {
+        /// <summary>
+        /// Translates a string from one language to another.
+        /// </summary>
+        /// <param name="input">The text to translate</param>
+        /// <param name="from">The language the text is in</param>
+        /// <param name="to">The language to translate the text to</param>
+        /// <returns>A translated string</returns>
+        /// <exception cref="InvalidOperationException">This isn't implemented yet.</exception>
+        static string Translate(this string input, Lang from, Lang to)
+        {
+            // TODO: Add code
+            throw new InvalidOperationException();
+        }
+    }
+}
